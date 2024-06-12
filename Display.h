29091f8712bf9d577e6c6d723d49e90be5a55a08
Matúš Ollah <matusollah420@gmail.com>
2024-06-12 19:39:53 +0200

    add space

--- Display.h
+++ Display.h
@@ -12,6 +12,7 @@ void updateDisplay() {
     display.println(String(RIG_IDENTIFIER));
 
     display.setTextSize(1);
+    display.println();
     display.println("Hashrate: " + String(hashrate / 1000) + " kH/s");
     display.println("Difficulty: " + String(difficulty / 100));
     display.println("Shares: " + String(share_count));
